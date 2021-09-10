2a52a1567aa5641ca30b239ac014709416c5144a
edaouda <88290755+edaouda@users.noreply.github.com>
2021-09-10 23:03:55 +0200

    version 2
    delete search options, add number of cell

--- index.php
+++ index.php
@@ -16,22 +16,18 @@
 <body>
     <div class="main_layout">
 		<header>
-			<div id="numImage"></div>
-			<div class="search">
-				<input type="text" id="search-term" />
-				<button id="search"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg></button>
-			</div>
+			<h2>Image n°<b id="numImage"></b></h2>
 		</header>
         <div class="grille" >
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
-            <div class="case cache"></div>
+            <div class="case cache"><p class="numCase">1</p></div>
+            <div class="case cache"><p class="numCase">2</p></div>
+            <div class="case cache"><p class="numCase">3</p></div>
+            <div class="case cache"><p class="numCase">4</p></div>
+            <div class="case cache"><p class="numCase">5</p></div>
+            <div class="case cache"><p class="numCase">6</p></div>
+            <div class="case cache"><p class="numCase">7</p></div>
+            <div class="case cache"><p class="numCase">8</p></div>
+            <div class="case cache"><p class="numCase">9</p></div>
         </div>
         <div class="options">
 			<div class="btnPasserEtape">
